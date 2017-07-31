8a56c21fc908881a88b5deb846c5e3f41d9b814a
kkashuda <kkashuda@gmail.com>
2017-07-31 13:03:40 -0500

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+workout1 = Workout.create(
+  title: "HIIT Body Weight Workout",
+  duration: 12,
+  rest: 1, 
+  interval: 30, 
+  sets: 4 
+)
+
+workout1.exercises.create(description: "Jumping jacks")
+workout1.exercises.create(description: "High knees")
+workout1.exercises.create(description: "Jog in place")
+workout1.exercises.create(description: "Burpees")
+workout1.exercises.create(description: "Squat jumps")
+workout1.exercises.create(description: "Walk in place")
