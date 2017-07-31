6105e35ca31123f080b8c13e2fe2dac93d82e023
kkashuda <kkashuda@gmail.com>
2017-07-31 15:09:43 -0500

    add mapstatetoprops + connector

--- client/src/components/Workout.js
+++ client/src/components/Workout.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Header } from 'semantic-ui-react'
+import { connect } from 'react-redux';
+import Workouts from './Workouts';
 
 const Workout = () => {
+    debugger
+    const workouts = this.props.workouts;
     return (
         <div>
             <Header as='h3' block>
@@ -11,4 +15,14 @@ const Workout = () => {
     )
 }
 
-export default Workout;
+function mapStateToProps(state) {
+    if (state.workoutsReducer.length > 0) {
+        return {
+            workouts: state.workoutsReducer
+        }
+    } else {
+        workouts: [{title: '', duration: '', interval: '', rest: '', sets: ''}]
+    }
+}
+
+export default connect(mapStateToProps)(Workout);
---
+++ client/src/components/Workouts.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Workout from './Workout';
+
+class Workouts extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default Workouts;
