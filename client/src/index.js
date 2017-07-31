205484ee2c074baac6de4b56b5e828e3f3fde3e6
kkashuda <kkashuda@gmail.com>
2017-07-31 13:22:42 -0500

    create actions and reducers directory + files and create store

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,24 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.css'
+import { createStore, applyMiddleware } from 'redux'
+import reducers from './reducers/reducer'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux';
+import { getWorkouts } from './actions/workoutActions';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)  
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+);
+
 registerServiceWorker();
