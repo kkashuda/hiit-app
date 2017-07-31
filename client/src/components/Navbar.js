262416220183deae4dfbd7f246ef9ac582d93b4d
kkashuda <kkashuda@gmail.com>
2017-07-31 14:50:18 -0500

    Add workouts route + set up component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,33 +1,32 @@
 import React from 'react'; 
 import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider, Menu } from 'semantic-ui-react';
-
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
 import Homepage from './Homepage'
+import Workout from './Workout'
 
 const Navbar = () => {
-
 return (
-<Router>
-    <div>
-      <Header icon textAlign='center'>
-        <Icon.Group size='big'>
-        <Icon size='small' name='spinner' color='pink' />
-        </Icon.Group>
-      </Header>
-    <Menu>
-      <Menu.Item as={Link} to='/'>
-        Home
-      </Menu.Item>
-    </Menu>
-
-    <Route exact path="/" component={Homepage}/>
-</div>
-
-  </ Router >
+    <Router>
+        <div>
+            <Header icon textAlign='center'>
+                <Icon.Group size='big'>
+                    <Icon size='small' name='spinner' color='pink' />
+                </Icon.Group>
+            </Header>
+            
+            <Menu>
+                <Menu.Item as={Link} to='/'> Home </Menu.Item>
+                <Menu.Item as={Link} to='/workouts'> Workouts </Menu.Item>
+            </Menu>
+            
+            <Route exact path="/" component={Homepage}/>
+            <Route exact path="/workouts" component={Workout}/>
+        </div>
+    </ Router >
 )}
 
 export default Navbar;
