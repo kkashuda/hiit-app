97a90a9b21f733197933db76dba60a047d8998ec
kkashuda <kkashuda@gmail.com>
2017-07-31 14:53:46 -0500

    Export workout component + add a header

--- client/src/components/Workout.js
+++ client/src/components/Workout.js
@@ -1,8 +1,14 @@
-import React from 'react'; 
+import React from 'react'
+import { Header } from 'semantic-ui-react'
 
 const Workout = () => {
     return (
         <div>
+            <Header as='h3' block>
+                HIIT WORKOUTS:
+            </Header>
         </div>
     )
 }
+
+export default Workout;
