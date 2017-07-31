a450d6cd94996ea821bfdfca33a3ee9895ee007c
kkashuda <kkashuda@gmail.com>
2017-07-31 13:33:06 -0500

    create Workouts Api + define getWorkouts()

---
+++ client/src/components/WorkoutApi.js
@@ -0,0 +1,15 @@
+class WorkoutApi {
+    static getWorkouts() {
+        return fetch('/workouts', {
+            method: "GET", 
+            headers: {
+                'Accepts': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(response => response.json())
+        .catch(error => console.log(error))
+    }
+}
+
+export default WorkoutApi;
