1b3933327922c5b3bf3ed4cc5e25e0af5fdee4fb
kkashuda <kkashuda@gmail.com>
2017-08-01 16:06:02 -0500

    Create workout form

--- client/src/components/WorkoutForm.js
+++ client/src/components/WorkoutForm.js
@@ -2,3 +2,44 @@ import React, { Component } from 'react'
 import App from '../App'
 import Exercise from './Exercise'
 
+class WorkoutForm extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            workouts: [],
+        }
+
+        this.createWorkout = this.createWorkout.bind(this);
+    }
+
+    createWorkout(workout) {
+        return fetch('/workouts', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(workout)
+        })
+         .then(response => response.json())
+         .then(workout => this.setState({
+             workouts: this.state.workouts.concat(workout)
+         }))
+        .then(window.location.reload())
+    }
+
+    render() {
+        return (
+            <div>
+                <form ref="create-workout" className="workout-form" onSubmit = {(event) => this.handleSubmit(event)}>
+                <input type="text" 
+                       placeholder="title"
+                       onChange={event => this.handleTitleChange(event)}
+                       value={this.props.title}
+
+                 />
+                </form>
+            </div>
+        )
+    }
+}
