0bcd96b65b4265e5b624ef1f62cb343fe0cc0059
kkashuda <kkashuda@gmail.com>
2017-08-03 13:59:24 -0500

    Turn workouts component into a stateless component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { render } from 'react-dom';
 import './App.css';
 import Navbar from './components/Navbar'
 import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider } from 'semantic-ui-react'
 
 const App = () => (
-  render(
       <div className="App">
         <Navbar />
       </div>
-))
+)
+
+render (<App />, document.getElementById('root'))
 
 export default App;
--- client/src/components/Workouts.js
+++ client/src/components/Workouts.js
@@ -2,49 +2,42 @@ import React, { Component } from 'react';
 import Workout from './Workout';
 import { Container, Header, Segment, Button, Icon, Dimmer, Loader, Divider} from 'semantic-ui-react'
 
-class Workouts extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() { 
-        return (
-            <div>
-                {this.props.workouts.map((workout, index) => 
-                <Container>
+const Workouts = props => (
+    <div>
+        {props.workouts.map((workout, index) => 
+        <Container>
             <Segment color="pink">
-                    <div key={workout.id}>
-                        <Header as='h2'> {workout.title} </Header>
-                        <p> Total duration: {workout.duration} minutes </p>
-                        <p></p>
-                        <p> Do each workout for {workout.interval} seconds</p>
-                        <p> After each workout rest for {workout.interval} seconds</p>
+                <div key={workout.id}>
+                    <Header as='h2'> {workout.title} </Header>
+                    <p> Total duration: {workout.duration} minutes </p>
+                    <p></p>
+                    <p> Do each workout for {workout.interval} seconds</p>
+                    <p> After each workout rest for {workout.interval} seconds</p>
 
-                        <p> Complete the circuit {workout.sets} x </p>
-                        <p> After each set rest for {workout.rest} minute(s) </p>
-                       
-                    </div>
+                    <p> Complete the circuit {workout.sets} x </p>
+                    <p> After each set rest for {workout.rest} minute(s) </p>    
+                </div>
                     
-                    <br></br> <Header as='h3'> Exercises:</Header>
-                    <div>
-                    
-                        { workout.exercises.split(",").map((exercise, index) => 
+                <br></br> <Header as='h3'> Exercises:</Header>
+                <div>
+                    {workout.exercises.split(",").map((exercise, index) => 
                         <div key={exercise.id}>
                              <p> {exercise} </p>
                         </div>
                     )}
-                    </div>
-                      <br></br>
-                             </Segment>
-                    </Container>
-             
-                  
-                 )}
+                </div>
+                <br></br>
+            </Segment>
+        </Container>
+            )
+        }
+        </div>
+    )
 
-    
-            </div>
-        )
+
+    Workouts.defaultProps = {
+        workouts: []
     }
-}
+    
 
 export default Workouts;
